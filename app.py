5f6bd108906cc496f8e7db8827ab2eb0ba3e4973
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2023-09-26 02:07:03 +0530

    main

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
+# Fixed directory containing PDF documents
+pdf_directory = "data/"
+
 # Function to list PDF files in a directory
 def list_pdf_files(directory):
     pdf_files = []
@@ -17,7 +20,28 @@ def list_pdf_files(directory):
             pdf_files.append(os.path.join(directory, filename))
     return pdf_files
 
-# ...
+# Function to extract data from all the PDF documents
+def get_data(docs):
+    data = ""
+    for doc in docs:
+        pdf_reader = PdfReader(doc)
+        for page in pdf_reader.pages:
+            data += page.extract_text()
+    return data
+
+# Convert data to chunks
+def get_chunks(raw_data):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(raw_data)
+    return chunks
+
+# ... (other functions remain the same)
 
 def main():
     load_dotenv()
@@ -26,7 +50,7 @@ def main():
         st.write("OPENAI_API_KEY is not set. Please add your key in .env file.")
         exit(1)
 
-    st.set_page_config(page_title="Knowledge Base Chatbot", page_icon="💬")
+    st.set_page_config(page_title="Ayurvedic Doctor Chatbot", page_icon="💬")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -34,7 +58,7 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Ayurvedic-Doctor-Bot 💬")
+    st.header("Ayurvedic Doctor Chatbot 💬")
 
     with st.chat_message("assistant"):
         st.write("Hello👋, How can I help you today?")
@@ -46,30 +70,26 @@ def main():
             st.write(user_input)
         handle_user_input(user_input)
 
-    # Add a sidebar for specifying the directory containing PDF files
-    with st.sidebar:
-        st.subheader("Directory with PDF documents")
-        pdf_directory = st.text_input("Enter the path to the directory:")
-        if pdf_directory and st.button("Process"):
-            pdf_files = list_pdf_files(pdf_directory)
-            if not pdf_files:
-                st.warning("No PDF files found in the specified directory.")
-            else:
-                with st.spinner("Processing..."):
-                    # Get data from the documents in the directory
-                    raw_data = get_data(pdf_files)
+    # Process PDF files from the fixed directory
+    pdf_files = list_pdf_files(pdf_directory)
+    if not pdf_files:
+        st.warning("No PDF files found in the specified directory.")
+    else:
+        with st.spinner("Processing..."):
+            # Get data from the documents in the directory
+            raw_data = get_data(pdf_files)
 
-                    # Divide data into chunks
-                    chunks = get_chunks(raw_data)
+            # Divide data into chunks
+            chunks = get_chunks(raw_data)
 
-                    # Convert to embeddings and create a vector store
-                    vectorstore = get_vectorstore(chunks)
+            # Convert to embeddings and create a vector store
+            vectorstore = get_vectorstore(chunks)
 
-                    if vectorstore:
-                        st.caption("Processing Completed!")
+            if vectorstore:
+                st.caption("Processing Completed!")
 
-                    # Create a conversation chain (user input)
-                    st.session_state.conversation = get_conversation_chain(vectorstore)
+            # Create a conversation chain (user input)
+            st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == '__main__':
     main()
